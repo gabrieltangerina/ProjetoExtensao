cf0f92e10a0033bc71c04c59ecf0f6e96ccf6454
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2023-09-14 19:29:24 -0300

    Função para postagem de conteúdo

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,6 @@
 const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu(){
+function toggleMenu() {
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
@@ -9,7 +9,7 @@ btnMobile.addEventListener('click', toggleMenu);
 
 // ------------- Função que calcula o tempo -------------
 
-function calcularTempoPassado(data){
+function calcularTempoPassado(data) {
     const dataPost = new Date(data);
     const dataAtual = new Date();
 
@@ -22,15 +22,15 @@ function calcularTempoPassado(data){
     const meses = Math.floor(dias / 30);
 
 
-    if(meses > 0){
+    if (meses > 0) {
         return `${meses}m`
-    }else if(dias > 0){
+    } else if (dias > 0) {
         return `${dias}d`;
-    }else if(horas > 0){
+    } else if (horas > 0) {
         return `${horas}h`;
-    }else if(minutos > 0){
+    } else if (minutos > 0) {
         return `${minutos}min`;
-    }else{
+    } else {
         return `${segundos}s`;
     }
 }
@@ -38,15 +38,15 @@ function calcularTempoPassado(data){
 // ------------------ IntersectionObserver ------------------
 const conteudos = document.querySelectorAll(".hidden");
 
-const myObserver = new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        if(entry.isIntersecting){
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
             entry.target.classList.add('show');
         }
     })
 });
 
-conteudos.forEach((conteudo)=> myObserver.observe(conteudo));
+conteudos.forEach((conteudo) => myObserver.observe(conteudo));
 
 // ------------------ SLIDE 1 ------------------
 const slider1 = document.querySelectorAll('.imagens-slide1');
@@ -55,48 +55,51 @@ const btnNext1 = document.getElementById('next-button1');
 const btnPrevMobile = document.getElementById('prev-button-mobile');
 const btnNextMobile = document.getElementById('next-button-mobile');
 
-let currentSlide = 0;
-
-function hideSlider(){
-    slider1.forEach(item => item.classList.remove('on'));
-}
-
-function showSlider(){
-    slider1[currentSlide].classList.add('on');
-}
-
-function nextSlider(){
-    hideSlider();
-    if(currentSlide === slider1.length - 1){
-        currentSlide = 0;
-    }else{
-        currentSlide++;
-    }
-    showSlider();
-}
-
-function prevSlider(){
-    hideSlider();
-    if(currentSlide === 0){
-        currentSlide = slider1.length - 1;
-    }else{
-        currentSlide--;
-    }
-    showSlider();
-}
-
-btnNext1.addEventListener('click', nextSlider)
-btnPrev1.addEventListener('click', prevSlider)
-
-btnPrevMobile.addEventListener('click', prevSlider)
-btnNextMobile.addEventListener('click', nextSlider)
+criaConteudo(slider1, btnPrev1, btnNext1, btnPrevMobile, btnNextMobile);
 
 const tempoPost1HTML = document.querySelector(".tempoDecorrido");
-
 let dataPost1 = "2023-09-09T15:47:00";
 let tempoPassadoPost1 = calcularTempoPassado(dataPost1);
-
 tempoPost1HTML.innerHTML = tempoPassadoPost1;
+
+
+function criaConteudo(imagens, botaoPrev, botaoNext, btnMobilePrev, btnMobileNext) {
+    let currentSlide = 0;
+    
+    function hideSlider() {
+        imagens.forEach(item => item.classList.remove('on'));
+    };
+
+    function showSlider() {
+        imagens[currentSlide].classList.add('on');
+    };
+    
+    function nextSlider() {
+        hideSlider();
+        if (currentSlide === imagens.length - 1) {
+            currentSlide = 0;
+        } else {
+            currentSlide++;
+        }
+        showSlider();
+    };
+    
+    function prevSlider() {
+        hideSlider();
+        if (currentSlide === 0) {
+            currentSlide = imagens.length - 1;
+        } else {
+            currentSlide--;
+        }
+        showSlider();
+    };
+    
+    botaoNext.addEventListener('click', nextSlider);
+    botaoPrev.addEventListener('click', prevSlider);
+    
+    btnMobilePrev.addEventListener('click', nextSlider);
+    btnMobileNext.addEventListener('click', prevSlider);
+}
 
 
 // ------------------ SLIDE 2 ------------------
@@ -107,87 +110,4 @@ const btnNext2 = document.getElementById('next-button2');
 const btnPrevMobile2 = document.getElementById('prev-button-mobile2');
 const btnNextMobile2 = document.getElementById('next-button-mobile2');
 
-
-let currentSlide2 = 0;
-
-function hideSlider2(){
-    slider2.forEach(item => item.classList.remove('on'));
-}
-
-function showSlider2(){
-    slider2[currentSlide2].classList.add('on');
-}
-
-function nextSlider2(){
-    hideSlider2();
-    if(currentSlide2 === slider2.length - 1){
-        currentSlide2 = 0;
-    }else{
-        currentSlide2++;
-    }
-    showSlider2();
-}
-
-function prevSlider2(){
-    hideSlider2();
-    if(currentSlide2 === 0){
-        currentSlide2 = slider2.length - 1;
-    }else{
-        currentSlide2--;
-    }
-    showSlider2();
-}
-
-
-btnNext2.addEventListener('click', nextSlider2)
-btnPrev2.addEventListener('click', prevSlider2)
-
-btnPrevMobile2.addEventListener('click', prevSlider2)
-btnNextMobile2.addEventListener('click', nextSlider2)
-
-// ------------------ SLIDE 3 ------------------
-const slider3 = document.querySelectorAll('.imagens-slide3');
-const btnPrev3 = document.getElementById('prev-button3');
-const btnNext3 = document.getElementById('next-button3');
-
-const btnPrevMobile3 = document.getElementById('prev-button-mobile3');
-const btnNextMobile3 = document.getElementById('next-button-mobile3');
-
-
-let currentSlide3 = 0;
-
-function hideSlider3(){
-    slider3.forEach(item => item.classList.remove('on'));
-}
-
-function showSlider3(){
-    slider3[currentSlide3].classList.add('on');
-}
-
-function nextSlider3(){
-    hideSlider3();
-    if(currentSlide3 === slider3.length - 1){
-        currentSlide3 = 0;
-    }else{
-        currentSlide3++;
-    }
-    showSlider3();
-}
-
-function prevSlider3(){
-    hideSlider3();
-    if(currentSlide3 === 0){
-        currentSlide3 = slider3.length - 1;
-    }else{
-        currentSlide3--;
-    }
-    showSlider3();
-}
-
-
-btnNext3.addEventListener('click', nextSlider3)
-btnPrev3.addEventListener('click', prevSlider3)
-
-btnPrevMobile3.addEventListener('click', nextSlider3)
-btnNextMobile3.addEventListener('click', prevSlider3)
-
+criaConteudo(slider2, btnPrev2, btnNext2, btnPrevMobile2, btnNextMobile2);
